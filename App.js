c4019fc9a7bbae33b497d8f1319f4cf8264ae98c
Waqaruddin <syedwaqar.uddin@outlook.com>
2020-09-03 21:17:20 -0400

    Created Flatlist

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+ import { FlatList, StyleSheet, Text, View } from 'react-native';
+ import { setStatusBarBackgroundColor } from 'expo-status-bar';
 
-export default function App() {
+
+const DATA = [
+{ title: 'Flight1', source:'Kentucky', destination:'Chicago', totalPassengers: '100 passengers', departure:'Departing' , id: '1'},
+{ title: 'Flight2', source:'New York', destination:'Los Angeles', totalPassengers: '300 passengers', departure: 'Arriving',id: '2'},
+{ title: 'Flight3',source:'Los Vegas', destination:' Dallas', totalPassengers: '260 passengers', departure:"Arriving", id: '3'},
+{ title: 'Flight4',source:'Chicago', destination:' Houston', totalPassengers: '550 passengers', departure:"Arriving", id: '4'},
+
+];
+
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style = {styles.titleText}> Flight Status</Text>
+
+      <FlatList
+      data ={DATA}
+
+      renderItem = {({item}) =><Text style = {styles.textStyle}> {item.title}, {item.source}  to  {item.destination}  {'\n'} {item.totalPassengers} {'\n'} {item.departure} </Text>}
+
+
+
+      keyExtractor = {item => item.id}
+        
+      />
     </View>
   );
 }
 
+export default App;
+
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex: 1,
+   paddingTop: 22,
+   fontSize:20,
+   textAlign:'center',
+   justifyContent:'center',
+   alignContent:'center'
+   
   },
+  textStyle: {
+    flex: 1,
+    paddingTop: 22,
+    padding:30,
+    fontSize:20,
+    textAlign:'center',
+    justifyContent:'center',
+    alignContent:'center',
+    backgroundColor: '#ADD8E6',
+    borderWidth: 3
+
+  },
+  titleText:{
+
+    fontWeight: 'bold',
+    fontSize: 32,
+    textAlign:'center',
+    justifyContent:'center',
+    alignContent:'center',
+    paddingTop:22
+    
+}
+  
+
+
 });
+
+
+
+
+
+
+
+
+
